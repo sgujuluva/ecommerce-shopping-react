994b1555e62b22e0a42dba673dbe881ad4a53cf6
sgujuluva <sanghee04@gmail.com>
2022-08-02 19:42:00 +0200

    header items created

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <div className="header-logo">
+    </div>
+    <div className="header-search">
+
+    </div>
+    <div className="header-nav">
+    <div className="nav-item">
+    <span className = "nav-item-firstline">Hello Guest</span>
+    <span className = "nav-item-secondline">Sign In</span>
+    </div>
+    <div className="nav-item">
+    <span className = "nav-item-firstline">Hello Guest</span>
+    <span className = "nav-item-secondline">Sign In</span>
+        </div>
+        <div className="nav-item">
+        <span className = "nav-item-firstline">Hello Guest</span>
+    <span className = "nav-item-secondline">Sign In</span>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
