d1f5d0f161a635cbff5b5b41e29c7c360a636fba
sgujuluva <sanghee04@gmail.com>
2022-08-11 15:10:46 +0200

    routing for home and checkout page using routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 //components
-import Header from "./components/Header"
+import Header from "./components/Header";
 import Home from "./components/Home";
-import Products from "./components/Products"
+import Checkout from "./components/Checkout";
 //styles
-import './App.css';
-import "./Header.css"; 
+import "./App.css";
+import "./Header.css";
 import "./Home.css";
 import "./Products.css";
+//router
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     Eshop website
-     <Header/>
-     <Home/>
-  
+      <Routes>
+        Eshop website
+     
+        <Route path="/" element={<><Header/><Home/></>}/>
+        <Route path="/Checkout" element={<><Header /> <Checkout /></>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Checkout/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//styles
+/* import "./Checkout.css"; */
+
+function Checkout() {
+  return (
+    <div className="checkout-container">
+    
+    Hello world
+      </div>
+  )
+}
+
+export default Checkout
