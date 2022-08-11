62e6079ba76d99f18dda9bdb67492fd064782a1b
sgujuluva <sanghee04@gmail.com>
2022-08-11 12:06:06 +0200

    rating done

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 //images
-import HandBagImage from "../../images/handbag.png"
+import HandBagImage from "../../images/handbag.png";
 
-function Products({id,title,price,rating,image}) {
+function Products({ id, title, price, rating, image }) {
   return (
     <div className="product-container">
       <div className="product-info">
         <p className="product-title">{title}</p>
         <p className="product-price">{price},-€</p>
-        <div className="product-rating">⭐️⭐️⭐️⭐️</div>
-        <img src={image}alt="hand bag " />
+        <div className="product-rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐️</p>
+            ))}
+        </div>
+        <img src={image} alt="hand bag " />
         <button>Add to Basket</button>
       </div>
     </div>
