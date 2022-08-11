6bb12c8cca13abe6710462458ab49e83a53f57c0
sgujuluva <sanghee04@gmail.com>
2022-08-11 11:17:06 +0200

    home page styled

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -6,9 +6,9 @@ function Products() {
   return (
     <div className="product-container">
       <div className="product-info">
-        <p className="product-title">Title</p>
+        <p className="product-title">Lenovo Laptop Shoulder Bag T210, 15.6-Inch Laptop or Tablet, Sleek, Durable and Water-Repellent Fabric, Lightweight Toploader, Business Casual or School, GX40Q17229, Grey</p>
         <p className="product-price">30,-€</p>
-        <div className="product-rating">⭐️</div>
+        <div className="product-rating">⭐️⭐️⭐️⭐️</div>
         <img src={HandBagImage}alt="hand bag " />
         <button>Add to Basket</button>
       </div>
