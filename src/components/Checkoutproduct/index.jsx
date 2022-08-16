793f7f4d762666efdb9a7ec2750bc39bd82a17bf
sgujuluva <sanghee04@gmail.com>
2022-08-16 11:00:24 +0200

    removing the element by id

--- src/components/Checkoutproduct/index.jsx
+++ src/components/Checkoutproduct/index.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+//context
+import { Cart } from "../../context/Context";
 
-function Checkoutproducts({id,title,price,rating,image}) {
+function Checkoutproducts({ id, title, price, rating, image }) {
+  const { state, dispatch } = useContext(Cart);
+  const removeFromCart = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: "id",
+    });
+  };
   return (
     <div className="checkoutprod-container">
       <div className="checkoutprod-info">
@@ -9,11 +18,18 @@ function Checkoutproducts({id,title,price,rating,image}) {
           <p className="checkprod-title">{title}</p>
           <br />
           <p className="checkprod-price">{price}</p>
-          <div className="checkout-rating">{rating}</div>
+          <div className="checkout-rating">
+            {rating}
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p>⭐️</p>
+              ))}
+          </div>
         </div>
       </div>
 
-      <button>Remove from Basket</button>
+      <button onClick={removeFromCart}>Remove from Basket</button>
     </div>
   );
 }
--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -1,12 +1,11 @@
 export const ACTIONS = {
   ADD_TO_CART: "ADD_TO_CART",
+  REMOVE_FROM_CART : " REMOVE_FROM_CART"
 };
 //total amount of the products in subtotal page
 export const getCartTotal = (cart) => {
-    return (cart?.reduce((amount,item) => item.price + amount, 0 ))
-}
-
-
+  return cart?.reduce((amount, item) => item.price + amount, 0);
+};
 
 const cartReducer = (state, action) => {
   console.log(state);
@@ -17,6 +16,11 @@ const cartReducer = (state, action) => {
         ...state,
         cart: [...state.cart, action.item],
       };
+      case ACTIONS.REMOVE_FROM_CART:
+        return {
+            ...state,
+            cart:[]
+        }
     default:
       return state;
   }
